3d4aaab6b0a7f5d3df595b956a20889fdf962d7b
emcmanus11 <elyse_mcmanus@brown.edu>
2014-09-23 22:02:00 -0400

    Changed selection to not select on at first

--- no-click.js
+++ no-click.js
@@ -1,25 +1,20 @@
 $(document).ready(function() {
 	// Mouseenter
 	$("#dynamic, #static").on("mouseenter", "a, button, .special-click", function(e) {
-		//alert("oi");
-		console.log("Target entered");
 		enter_target(e, this);
-
 	});
 
 	// Mouseleave
 	$("#dynamic, #static").on("mouseleave", "a, button, .special-click", function(e) {
-		//alert("oi");
-		console.log("Target exited");
 		exit_target(e, this);
 	});
-	append_links();
+	//append_links();
 });
 
 function append_links() {
 	$("#dynamic").append("<a href='http://www.google.com'>Dynamic link to google</a>");
 	$("#dynamic").append("<br>");
-	$("#dynamic").append("<button onclick='addButton()'>Dynamic button</button>");
+	$("#dynamic").append("<button onclick='addButton()'>Make more buttons</button>");
 	$("#dynamic").append("<div class='special-click' onclick='random_function(\"HI\")'>Dynamic div with class</div>");
 }
 
@@ -28,26 +23,27 @@ function goToLink () {
 }
 
 function addButton () {
-	$("#dynamic").append("<button onclick='goToLink()'>Hi I'm a button</button>");
+	$("#dynamic").append("<button onclick='goToLink()'>Go to imgur.com</button>");
 }
 
 function exit_target (e, elt) {
 	var state = $(elt).attr("data-state");
 	// From entered to leaving (red to blue)
-	if(state == undefined || state == 0){
+	clearTimeout($(elt).attr("data-time-outer"));
+	if(state == 1){
 		console.log("leave first time");
 		$(elt).css("background-color", "blue");
-		$(elt).attr("data-state", 1);
+		$(elt).attr("data-state", 2);
 		var time = setTimeout(function(){
 			var color = $(elt).attr("data-bg-color");
 			$(elt).css("background-color", color);
 			$(elt).attr("data-state", 0);
 		}, 3000);
-		$(elt).attr("data-all", time);
+		$(elt).attr("data-time", time);
 	} else {
 	// From re-entered to leaving (blue to original color)
 		console.log("leave 2nd time");
-		clearTimeout($(elt).attr("data-all"));
+		clearTimeout($(elt).attr("data-time"));
 		var color = $(elt).attr("data-bg-color");
 		$(elt).css("background-color", color);
 		$(elt).attr("data-state", 0);
@@ -62,30 +58,22 @@ function enter_target (e, elt) {
 	var state = $(elt).attr("data-state");
 	// From unentered / not selected at all to initial entry (original color to red)
 	if(state == undefined || state == 0){
-		console.log("entered first time");
-		var color = $(elt).css("background-color");
-		$(elt).attr("data-bg-color", color);
-		console.log($(elt).attr("data-bg-color"));
-		$(elt).css("background-color", "red");
-		console.log($(elt).attr("data-bg-color"));
-		
-		var time = setTimeout(function(){
-			$(elt).css("background-color", color);
-			console.log("color2");
-			console.log(color);
-		}, 3000);
-		$(elt).attr("data-all", time);
+		var time_outer = setTimeout(function(){
+			var color = $(elt).css("background-color");
+			$(elt).attr("data-bg-color", color);
+			$(elt).css("background-color", "red");
+			$(elt).attr("data-state", 1);
+			var time = setTimeout(function(){
+				$(elt).css("background-color", color);
+			}, 3000);
+			$(elt).attr("data-time", time);
+		}, 250);
+		$(elt).attr("data-time-outer", time_outer);
 	} else {
 	// From unentered to re-entered (blue to click to original color)
-		console.log("entered 2nd time");
-		clearTimeout($(elt).attr("data-all"));
-		$(elt).click();
-		// $(elt).attr("data-bg-color", $(elt).css("background-color"));
+		clearTimeout($(elt).attr("data-time"));
 		var color = $(elt).attr("data-bg-color");
 		$(elt).css("background-color", color);
-		
-		//$(elt).attr("data-state", 0);
-		console.log("have color");
-		console.log(color);
+		elt.click();
 	}
 }
